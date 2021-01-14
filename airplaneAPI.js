b9995b414ddfa5aa763f3d6da8289eab16f92a3d
kevin <kevinhellman94@gmail.com>
2021-01-13 20:57:57 -0500

    Added get from local storage

--- airplaneAPI.js
+++ airplaneAPI.js
@@ -1,4 +1,7 @@
 $("document").ready(function () {
+
+    getLocal();
+
     // Format for each input:
         // country = "US"
         // currency = "USD"
@@ -16,11 +19,13 @@ $("document").ready(function () {
         var outboundDate = $("#outboundDate").val();
         var inboundDate = $("#inboundDate").val();
 
+        var queryURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/" + country + "/" + currency + "/" + locale + "/" + origin + "/" + destination + "/" + outboundDate + "/" + inboundDate,
+
+    //Using Skyscanner API to get flight information
     const settings = {
         "async": true,
         "crossDomain": true,
-
-        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/" + country + "/" + currency + "/" + locale + "/" + origin + "/" + destination + "/" + outboundDate + "/" + inboundDate,
+        "url": queryURL,
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "45049d9fb6msh17ba1e94f9859eep1817c7jsn5b1c2edaf89d",
@@ -31,5 +36,13 @@ $("document").ready(function () {
     $.ajax(settings).done(function (response) {
         console.log(response);
     });
+
+    function setLocal() {
+        
+    }
+
+    function getLocal(thisURl) {
+        queryURL = JSON.stringify(localStorage.getItem("flight"));
+    }
 
 });
