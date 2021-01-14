c37e6f3147ec920dcbb63a13e4ca4045d3d6d19f
alexhstrickland <alexhstrickland@gmail.com>
2021-01-13 20:54:56 -0500

    for loop added to console.log each hotel name

--- hotelAPI.js
+++ hotelAPI.js
@@ -11,19 +11,40 @@ $("document").ready(function(){
         }
     };
 
-    // function updatePage(response) {
-    //     var results = response;
-    //     var 
+    function updatePage(response) {
+        var results = response;
+        var hotels = response.suggestions[1].entities;
+        console.log(results);
+        console.log(hotels);
 
-
-    // for (var i=0; i <)
-    // }
+        for (var i = 0; i < hotels.length; i++) {
+            var hotelName = hotels[i].name;
+            console.log(hotelName);
+        }
+    }
     
     $.ajax(settings).done(function (response) {
         console.log(response);
-    });
+    }).then(function(response) {
+        updatePage(response)
+    })
+    ;
 
-
+    
+    // const settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=1178275040",
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-key": "9f5a975ae6mshbca34daf924fdecp158a23jsn28c8dc4d8933",
+    //         "x-rapidapi-host": "hotels4.p.rapidapi.com"
+    //     }
+    // };
+    
+    // $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    // });
 
 
 
