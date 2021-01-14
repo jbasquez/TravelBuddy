ed9e78c2a0baf684462c380bd410d0ed026fbff2
kevin <kevinhellman94@gmail.com>
2021-01-13 20:47:47 -0500

    Added URL Adjusted by User Input

--- airplaneAPI.js
+++ airplaneAPI.js
@@ -1,23 +1,35 @@
 $("document").ready(function () {
+    // Format for each input:
+        // country = "US"
+        // currency = "USD"
+        // locale = "en-US"
+        // origin = "LAX-sky"
+        // destination = "SFO-sky"
+        // outboundDate = "2021-01-15"
+        // inboundDate = "2021-01-20"
 
-    var country = $("#country").val();
-    var currency = $("#currency").val();
-    var locale = $("#locale").val();
-    var origin = $("#origin").val();
-    var destination = $("#destination").val();
-    var outboundDate = $("#outboundDate").val();
-    var inboundDate = $("#inboundDate").val();
+        var country = $("#country").val();
+        var currency = $("#currency").val();
+        var locale = $("#locale").val();
+        var origin = $("#origin").val();
+        var destination = $("#destination").val();
+        var outboundDate = $("#outboundDate").val();
+        var inboundDate = $("#inboundDate").val();
 
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/LAX-sky/2021-01-22?inboundpartialdate=2021-01-22&outboundDate=2021-01-26",
+
+        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/LAX-sky/SFO-sky/2021-01-15/2021-01-20",
+
+        "url": "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/" + country + "/" + currency + "/" + locale + "/" + origin + "/" + destination + "/" + outboundDate + "/" + inboundDate,
         "method": "GET",
         "headers": {
-            "x-rapidapi-key": "h4hwyekXxImshM8IfqJdMeam7njTp1o5CQFjsnqXKP9CEM9iDD",
+            "x-rapidapi-key": "45049d9fb6msh17ba1e94f9859eep1817c7jsn5b1c2edaf89d",
             "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
         }
     };
+    
     $.ajax(settings).done(function (response) {
         console.log(response);
     });
