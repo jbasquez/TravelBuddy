a6a03bb1a2a1af9cbcdc7e7bb882ac80b3c06511
kevin <kevinhellman94@gmail.com>
2021-01-16 16:52:08 -0500

    Updated Information Appearance to look nicer

--- airplane.js
+++ airplane.js
@@ -1,51 +1,43 @@
 //flightInformation = [quotesPrice,thisCarrierOut,thisCarrierIn,thisDepartureDate,thisArrivalDate,directFlight]
 var flightInformation = getFlight();
-
+console.log(flightInformation);
 
 $("#chooseThis").on("click", function (event) {
     event.preventDefault();
     flightInformation = getAirlineInfo();
     console.log(flightInformation);
+    displayAirlineInfo(flightInformation);
+});
+
+function displayAirlineInfo(flightInformation) {
 
     for (var a=0; a<flightInformation.length; a++) {
         var infoPrice = $("<p class='flightInformation'>");
-        var infoDirect = $("<p class='flightInformation'>");
         var infoCarrierOut = $("<p class='flightInformation'>");
         var infoCarrierIn = $("<p class='flightInformation'>");
-        var infoDepart = $("<p class='flightInformation'>");
-        var infoArrive = $("<p class='flightInformation'>");
 
         infoPrice.attr("class", "flightInformation");
         var flightShow = $("#availableFlights");
 
         var cost = flightInformation[a].thisFlight;
-        infoPrice.text(cost);
+        var direct = flightInformation[a].direct;
+        infoPrice.text("Cost: $ " + cost + " Direct Flight: " + direct);
         flightShow.append(infoPrice);
 
-        var direct = flightInformation[a].direct;
-        infoDirect.text(direct);
-        flightShow.append(infoDirect);
-
         var outFly = flightInformation[a].carrierOut;
-        infoCarrierOut.text(outFly);
+        var depart = flightInformation[a].depart;
+        infoCarrierOut.text("Outbound Airline: " + outFly + " Leaving on: " + depart);
         flightShow.append(infoCarrierOut);
 
-        var leave = flightInformation[a].depart;
-        infoDepart.text(leave);
-        flightShow.append(infoDepart);
-
         var inFly = flightInformation[a].carrierIn;
-        infoCarrierIn.text(inFly);
-        flightShow.append(infoCarrierIn);
-
         var arrive = flightInformation[a].arrive;
-        infoArrive.text(arrive);
-        flightShow.append(infoArrive);
+        infoCarrierIn.text(inFly);
+        flightShow.append("Inbound Airline: " + inFly + " Leaving on: " + arrive);
 
         var horizontalRule = $("<hr style: color='yellow'>");
         flightShow.append(horizontalRule);
     }
-});
+}
 
 var flightStuff = getAirlineInfo();
 
