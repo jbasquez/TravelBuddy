26ca729db709fd4c4b29430ed6233f6f40668369
mdu830 <mdu830@yahoo.com>
2021-01-21 17:21:55 -0500

    adjusted start message to new fixed sidebar

--- airplaneAPI.js
+++ airplaneAPI.js
@@ -5,7 +5,7 @@ var thisQuery;
 
 // show starting message
 message = $("<h2>");
-message.attr("class", "m-1 ml-4 mr-4");
+message.attr("class", "m-1 ml-4 mr-4 p-5");
 message.text("Travel Buddy helps you find the best prices on flights and hotels!");
 $("#startMessage").append(message);
 
