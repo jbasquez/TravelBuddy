9b375a618c1cf2b4d9bad6dbf8ec097c68901dac
kevin <kevinhellman94@gmail.com>
2021-01-25 17:57:13 -0500

    Updating Comments

--- assets/js/airplane.js
+++ assets/js/airplane.js
@@ -15,6 +15,7 @@ function showSavedFlight() {
         var appendFlight = $(".savedFlight");
         appendFlight.empty();
 
+        //Place to show the stored Flight information
         var infoCard = $("<div>");
         infoCard.attr("class", "card saveCard ml-5 mt-2 mb-2 pl-5 pt-3 pr-5 pb-5");
         infoCard.attr("id", "flightsCard");
@@ -47,35 +48,41 @@ function showSavedFlight() {
         infoCard.append(carryIn2);
     }
 }
-
+//Append page buttons and set stylings and functions
 function appendButtons() {
-    // append page buttons
+ 
     preBtn = $("<button>");
     preBtn.attr("id", "previous");
     preBtn.attr("class", "mr-1 mt-5 btn btn-primary btn-sm");
     preBtn.text("Previous");
+    //Runs previousButton function
     preBtn.attr("onClick", "previousButton()");
     nxtBtn = $("<button>");
     nxtBtn.attr("id", "next");
     nxtBtn.attr("class", "ml-1 mt-5 btn btn-primary btn-sm");
+    //Runs nextButton function
     nxtBtn.attr("onClick", "nextButton()");
     nxtBtn.text("Next");
     $("#pageButtons").append(preBtn);
     $("#pageButtons").append(nxtBtn);
 }
 
-
+//Initiates the program with user input in place
 $("#searchBtn").on("click", function (event) {
+    //Emptying any irrelivant information to focus on the current query
     $("#errorPlace").empty();
     $("#pageButtons").empty();
     $("#startMessage").empty();
     $(".savedFlight").empty();
     $(".savedHotel").empty();
     event.preventDefault();
+    //runs the API
     getAirlineInfo();
     $("#startMessage").hide();
+    //Give time for the ajax call to run
     setTimeout(function () {
         flightInformation = returnFlight();
+        //If there are no flights found inform the user, otherwise show the flights
         if (flightInformation.length > 1) {
         displayAirlineInfo(flightInformation);
         appendButtons();
@@ -83,10 +90,11 @@ $("#searchBtn").on("click", function (event) {
             noFlights();
         }
     }, 2000);
+    //tells program to run Flight parts
     goHotel = false;
 });
 
-
+//Tells the user if no flights meet the criteria chosen
 function noFlights() {
     var flightArea = $("#flightsAndHotels");
     var noFly = $("<p>");
@@ -94,18 +102,22 @@ function noFlights() {
     flightArea.append(noFly);
 }
 
+//Shows the next set of Flights or Hotels
 function nextButton() {
+    //Runs for the flights
     if (goHotel === false) {
         $("html, body").animate({ scrollTop: 0 }, "slow");
+        //If there are flights to display, show them
         if (allFlights.length > 0) {
             next();
             flightInformation = returnFlight();
             displayAirlineInfo(flightInformation);
         }
     }
+    //Runs for the hotels
     if (goHotel === true) {
         $("html, body").animate({ scrollTop: 0 }, "slow");
-        
+        //If there are hotels to display, show them
         if (hotelArray.length > 0) {
             next();
             getMoreInfo();
@@ -113,7 +125,9 @@ function nextButton() {
     }
 }
 
+//Shows the previous set of Flights or Hotels
 function previousButton() {
+    //Runs for the flights
     if (goHotel === false) {
         $("html, body").animate({ scrollTop: 0 }, "slow");
         if (counter > 4) {
@@ -122,6 +136,7 @@ function previousButton() {
             displayAirlineInfo(flightInformation);
         }
     }
+    //Runs for the hotels
     if (goHotel === true) {
         $("html, body").animate({ scrollTop: 0 }, "slow");
         if (counter > 4) {
@@ -132,10 +147,12 @@ function previousButton() {
     }
 }
 
+//Appending the flight information
 function displayAirlineInfo(flightInformation) {
 
     var flightShow = $("#flightsAndHotels");
     
+    //Empty out old flight information
     if (flightInformation.length > 1) {
         flightShow.empty();
     }
@@ -145,7 +162,9 @@ function displayAirlineInfo(flightInformation) {
     titleH2.hide().fadeIn(700);
     flightShow.append(titleH2);
 
+    //Appending things to appropriate place
     for (var a = 0; a < flightInformation.length; a++) {
+        //Creates info card for flight information
         var infoCard = $("<div>");
         $("<div>").fadeIn(700);
         infoCard.attr("class", "card ml-2 mt-4 mr-2 mb-2 p-5");
@@ -159,24 +178,26 @@ function displayAirlineInfo(flightInformation) {
 
         infoPrice.attr("class", "flightInformation");
 
+        //Cost and whether flight is Direct
         var cost = flightInformation[a].thisFlight;
         var direct = flightInformation[a].direct;
         infoPrice.text("Cost: $ " + cost + " Direct Flight: " + direct);
         infoPrice.attr("value", cost + " " + direct);
         infoCard.append(infoPrice);
 
+        //Departure Airline and Date
         var outFly = flightInformation[a].carrierOut;
         var depart = flightInformation[a].depart;
-
         infoCarrierOut.text("Outbound Airline: " + outFly + " Leaving on: " + depart + " ");
         infoCard.append(infoCarrierOut);
 
-
+        //Arrival Airline and Date
         var inFly = flightInformation[a].carrierIn;
         var arrive = flightInformation[a].arrive;
         infoCarrierIn.text("Return Airline: " + inFly + " Leaving on: " + arrive);
         infoCard.append(infoCarrierIn);
 
+        //Save Button
         var button = $("<button>");
         button.attr("class", "mt-2 btn btn-light btn-sm clickToSave");
         button.attr("value", cost + "~" + direct + "~" + outFly + "~" + depart + "~" + inFly + "~" + arrive);
@@ -186,17 +207,21 @@ function displayAirlineInfo(flightInformation) {
     }
 }
 
+//Saves and formats information when User Clicks Save Button
 function saveFlight(saveThis) {
     storeFlight(saveThis);
     var getTheDay = saveThis.split("~");
 
+    //Formats day user leaves to use in Hotle API
     var leaveDay = getTheDay[3];
     inDay = leaveDay.slice(-2);
 
+    //Formats day user arrives to use in Hotel API
     var returnDay = getTheDay[5];
     outDay = returnDay.slice(-2);
 }
 
+//Changes program from Flights to Hotels
 function moveOn() {
     goHotel = true;
 }
--- assets/js/airplaneAPI.js
+++ assets/js/airplaneAPI.js
@@ -12,12 +12,14 @@ var inboundM;
 var errorPlace  = $("#errorPlace");
 var errorMessage = $("<p>");
 
+//Tells the program to move on to Hotel
 var goHotel = false;
 var outDay;
 var inDay;
 
 $("#loadingGif").hide();
 
+//Runs the ajax call for Flight information
 function getAirlineInfo() {
 
     // Format for each input:
@@ -29,7 +31,7 @@ function getAirlineInfo() {
     // var outboundDate = "2021-12 or 2021-12-31 or anytime";
     // var inboundDate = "2021-12-30 or 2021-12-31 or anytime";
 
-
+    //Gets user data for Chosen information and formats it
     var origin = $("#origin").val();
     origin = origin.toUpperCase();
     origin = origin + "-sky";
@@ -44,6 +46,7 @@ function getAirlineInfo() {
     inboundM = $("#inboundM").val();
     var inboundDate = (inboundY + "-" + inboundM).trim();
 
+    //Creates the link for the AJAX Call
     var queryURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/" + country + "/" + currency + "/" + locale + "/" + origin + "/" + destination + "/" + outboundDate + "/" + inboundDate;
 
     //Using Skyscanner API to get flight information
@@ -60,10 +63,7 @@ function getAirlineInfo() {
             "x-rapidapi-key": "45049d9fb6msh17ba1e94f9859eep1817c7jsn5b1c2edaf89d",
             "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
         },
-        // "success": function () {
-        //     $("#loadingGif").hide();
-
-        // },
+        //Displays information based on Error Code
         statusCode: {
             404: function() {
                 errorMessage.text("Server Not Found");
@@ -83,20 +83,25 @@ function getAirlineInfo() {
     });
 }
 
+//Show the flights based on where the user is in the aray
 function returnFlight() {
 
+    //Empties the old shown information
     allFlights = [];
 
     var options = thisQuery.Quotes.length;
 
-    //5 = 5
     current = counter;
-    //5 = 5             5 < 5+5                  5<30               5+1
+    //Keeps track of the users position the in the returned array
+    //Exits loop after 5 things are appended or the limit of 25 hotel options is reached
+    //5 = 5             5 < 10                  5<25               5+1
     for (counter = current; current < counter + 5 && current < options; current++) {
+        
         //Number
         var quotesPrice = thisQuery.Quotes[current].MinPrice;
         var thisCarrierOut = thisQuery.Quotes[current].InboundLeg.CarrierIds[0];
         var thisCarrierIn = thisQuery.Quotes[current].OutboundLeg.CarrierIds[0];
+
         //Assigns a name based on carrier code given for flight
         var carrierNames = thisQuery.Carriers.length;
         for (var i = 0; i < carrierNames; i++) {
@@ -111,14 +116,17 @@ function returnFlight() {
                 thisCarrierIn = thisQuery.Carriers[i].Name;
             }
         }
+
         //String
         var thisDepartureDate = thisQuery.Quotes[current].OutboundLeg.DepartureDate;
         var thisDepartureDate = thisDepartureDate.slice(0, 10);
         var thisArrivalDate = thisQuery.Quotes[current].InboundLeg.DepartureDate;
         var thisArrivalDate = thisArrivalDate.slice(0, 10);
+
         //Boolean
         var directFlight = thisQuery.Quotes[current].Direct;
 
+        //Object of Flight Information
         var thisFlight = {
             thisFlight: quotesPrice,
             carrierOut: thisCarrierOut,
@@ -130,19 +138,23 @@ function returnFlight() {
 
         allFlights.push(thisFlight);
     }
-
+    
+    //Returns a list of at most 5 available flights
     return allFlights;
 
 }
 
+//Removes in order to show 5 previous Flights or Hotels
 function previous() {
     counter = counter - 5;
 }
 
+//Adds in order to show 5 next Flights or Hotels
 function next() {
     counter = counter + 5;
 }
 
+//Saves the flight to local storage
 function storeFlight(flightInfo) {
     $("#pageButtons").empty();
     localStorage.setItem("flight", flightInfo);
